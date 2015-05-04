0f1f87b4b36494f2256c3a4762cbf597546edc4e
Jesse Shellabarger <shellajt@rose-hulman.edu>
2015-05-03 21:09:06 -0400

    Login Screen mostly done
    Still needs action Listener

--- src/GUI.java
+++ src/GUI.java
@@ -41,14 +41,14 @@ public class GUI {
 	public static void main(String[] args) {
 		GUI g = new GUI();
 		
-		Image image = new ImageIcon("resources/LoginBG.png").getImage();
+		Image image = new ImageIcon("resources/trackBackground.jpg").getImage();
 		ImagePanel panel = new ImagePanel(image);
 		g.activeFrames.get(0).getContentPane().add(panel);
 		
 		//Username Label
 		JLabel titleLabel = new JLabel();
 		titleLabel.setText("<html><b>Horse Racing</b><html>");
-		titleLabel.setForeground(Color.CYAN);
+		titleLabel.setForeground(Color.BLACK);
 		Font titleFont = titleLabel.getFont();
 		titleLabel.setFont(new Font(titleFont.getName(), 4, 14));
 		titleLabel.setSize(110, 25);
@@ -61,31 +61,60 @@ public class GUI {
 		//Username Label
 		JLabel usernameLabel = new JLabel();
 		usernameLabel.setText("<html><b>Username: </b><html>");
-		usernameLabel.setForeground(Color.CYAN);
+		usernameLabel.setForeground(Color.BLACK);
 		Font usernameFont = usernameLabel.getFont();
 		usernameLabel.setFont(new Font(usernameFont.getName(), 4, 14));
 		usernameLabel.setSize(110, 25);
 		panel.add(usernameLabel);
 		usernameLabel.setLocation(
 				panel.getWidth() / 2 - usernameLabel.getWidth(),
-				panel.getHeight() / 2 - usernameLabel.getHeight()*1/2);
+				panel.getHeight() / 2 - usernameLabel.getHeight()*3/2);
 		usernameLabel.setVisible(true);
 		
 		//Password Label
 		JLabel passwordLabel = new JLabel();
 		passwordLabel.setText("<html><b>Password: </b><html>");
-		passwordLabel.setForeground(Color.CYAN);
+		passwordLabel.setForeground(Color.BLACK);
 		Font passwordFont = passwordLabel.getFont();
 		passwordLabel.setFont(new Font(passwordFont.getName(), 4, 14));
 		passwordLabel.setSize(110, 25);
 		panel.add(passwordLabel);
 		passwordLabel.setLocation(
 				panel.getWidth() / 2 - passwordLabel.getWidth(),
-				panel.getHeight() / 2 + passwordLabel.getHeight() * 1/2);
+				panel.getHeight() / 2 - passwordLabel.getHeight() * 1/2);
 		passwordLabel.setVisible(true);
 		
 		//Username Textbox
+		JTextField usernameText = new JTextField();
+		usernameText.setSize(110, 25);
+		Font usernameBoxFont = usernameText.getFont();
+		usernameLabel.setFont(new Font(usernameBoxFont.getName(), 4, 14));
+		panel.add(usernameText);
+		usernameText.setLocation(panel.getWidth() / 2, panel.getHeight() / 2
+				- usernameLabel.getHeight() * 3/2);
+		usernameText.setVisible(true);
 		
+		// password Textbox
+		JTextField passwordText = new JTextField();
+		passwordText.setSize(110, 25);
+		Font passwordBoxFont = passwordText.getFont();
+		passwordLabel.setFont(new Font(passwordBoxFont.getName(), 4, 14));
+		panel.add(passwordText);
+		passwordText.setLocation(panel.getWidth() / 2 , panel.getHeight() / 2
+				- passwordLabel.getHeight() * 1 / 2);
+		passwordText.setVisible(true);
+		
+		// Set up Login Button
+		JButton loginButton = new JButton();
+		loginButton.setSize(110, 25);
+		loginButton.setText("Login");
+		loginButton.setLocation(
+				(panel.getWidth() / 2) - loginButton.getWidth() * 1/2,
+				(panel.getHeight() / 2) + (loginButton.getHeight()));
+		panel.add(loginButton);
+//		loginButton.addActionListener(new loginListener());
+		loginButton.setVisible(true);
+
 		
 		g.activeFrames.get(0).pack();
 		g.activeFrames.get(0).setVisible(true);
